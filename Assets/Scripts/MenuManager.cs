476c2e5fddfe1569562da376b9a45222700f2b87
Alteran0 <scottcanfield10@gmail.com>
2015-11-19 23:29:15 +0000

    Imported Main Menu
    Imported the main menu into the project, currently doesn't work due to
    some input manager errors?

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public Menu currentMenu;
+
+	public void Start()
+	{
+		ShowMenu(currentMenu);
+	}
+
+	public void ShowMenu(Menu menu)
+	{
+		if (currentMenu != null)
+			currentMenu.isOpen = false;
+
+
+		currentMenu = menu;
+		currentMenu.isOpen = true;
+
+
+	}
+	public void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (currentMenu.parent_menu != null)
+			{
+				currentMenu.isOpen = false;
+				currentMenu = currentMenu.parent_menu;
+				currentMenu.isOpen = true;
+			}
+
+		}
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+}
+
